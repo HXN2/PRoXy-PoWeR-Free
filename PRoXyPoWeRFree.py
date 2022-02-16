b609a724da0b36d3f08e8dce67d16ddf16109472
HEXiN @hxn_here  insta <98489657+HXN2@users.noreply.github.com>
2022-02-16 16:02:34 +0300

    Update PRoXyPoWeRFree.py

--- PRoXyPoWeRFree.py
+++ PRoXyPoWeRFree.py
@@ -1,1 +1,352 @@
 
+import os
+
+import time 
+
+import requests
+
+import socket
+
+from requests import get
+
+from re import findall
+
+import webbrowser
+
+url = "https://t.me/HEXiN1K"
+
+webbrowser.open(url)
+
+import requests
+
+import socket
+
+from requests import get
+
+from re import findall
+
+name = socket.gethostname()
+
+print("\n===========================================================")
+
+print("\n                    Hello sir " +name) 
+
+print("\n===========================================================")
+
+lib = input("""
+
+\n  من صنع هيكسن انستجرام @hxn_here
+
+\n  BY HeXiN INSTA @hxn_here
+
+\n  tarafından yapılmış HeXiN Instagram @hxn_here
+
+\n
+
+===========================================================
+
+\n   من صنع هيكسن تيليجرام @HEXiN 
+
+\n  BY HEXiN TELeGRaM @HEXiN1K
+
+\n  Bir telgraf HEXiN oluşturun @HEXiN1K
+
+\n ===========================================================
+
+ 
+
+ [1] DOWNLOAD LiP & UPDATE . تنزيل وتحديث المكاتب
+
+\n [2] SKiP DOWNLOAD & UPDATE LiP . تخطي تنزيل وتحديث المكاتب 
+
+\n [+] Please Choice >> """)
+
+if lib == "1":
+
+    try:
+
+        os.system('pip install requests')
+
+        os.system('pip install webbrowser')
+
+        os.system('pip install findall')       
+
+        os.system('pip install os')
+
+        os.system('pip install socket')
+
+        os.system("pip install get")
+
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        pass
+
+    except:
+
+        pass
+
+else:
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    pass
+
+from requests import get
+
+from re import findall
+
+print("""
+
+                _    ___   ___   _
+
+               | |  | \ \ / / \ | |
+
+               | |__| |\ V /|  \| |
+
+               |  __  | > < | . ` |
+
+               | |  | |/ . \| |\  |
+
+               |_|  |_/_/ \_\_| \_|
+
+  _ __  _ __ _____  ___   _
+
+ | '_ \| '__/ _ \ \/ / | | |
+
+ | |_) | | | (_) >  <| |_| |
+
+ | .__/|_|  \___/_/\ _\\__, |
+
+ | |                   __/ |
+
+ |_|                  |___/
+
+              
+
+              Daddy HEXiN is here @hxn_here
+
+""")
+
+from requests import get
+
+from re import findall
+
+import time as mm
+
+import sys as n
+
+def slow(M):
+
+	for c in M + '\n':		n.stdout.write(c)
+
+		n.stdout.flush()
+
+		mm.sleep(1. / 160)
+
+slow(""" Buy the special edition, head over to  Instagram @hxn_here  Good Bye.
+
+لشراء النسخة الخاصة توجة الى انستجرام @hxn_here الى اللقاء . """)
+
+S = input("""
+
+ 
+
+ ~ [1] Do you want to start searching for proxy? . 
+
+ هل تريد بدء البحث عن بروكسي ؟
+
+ ~ [2] Do you want Follow HEXiN in iNStAGrAM Baby ? .
+
+  هل تريد متابعة هيكسين على انستجرام بيبي ؟
+
+ 
+
+ [+] إختيارك رجاءً
+
+ [+] Please Choose: """)
+
+if S =='1':
+
+ from requests import get
+
+from re import findall
+
+urls = '''
+
+http://atomintersoft.com/transparent_proxy_list
+
+http://atomintersoft.com/anonymous_proxy_list
+
+http://atomintersoft.com/high_anonymity_elite_proxy_list
+
+http://atomintersoft.com/products/alive-proxy/proxy-list
+
+http://atomintersoft.com/products/alive-proxy/proxy-list?ap=9
+
+http://atomintersoft.com/products/alive-proxy/proxy-list/3128
+
+http://atomintersoft.com/products/alive-proxy/proxy-list/com
+
+http://atomintersoft.com/products/alive-proxy/proxy-list/high-anonymity/
+
+http://atomintersoft.com/products/alive-proxy/socks5-list
+
+http://atomintersoft.com/proxy_list_domain_com
+
+http://atomintersoft.com/proxy_list_domain_edu
+
+http://atomintersoft.com/proxy_list_domain_net
+
+https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks4.txt
+
+http://atomintersoft.com/proxy_list_domain_org
+
+http://atomintersoft.com/proxy_list_port_3128
+
+http://atomintersoft.com/proxy_list_port_80
+
+http://atomintersoft.com/proxy_list_port_8000
+
+http://atomintersoft.com/proxy_list_port_81
+
+http://hack-hack.chat.ru/proxy/allproxy.txt
+
+http://hack-hack.chat.ru/proxy/anon.txt
+
+http://hack-hack.chat.ru/proxy/p1.txt
+
+http://hack-hack.chat.ru/proxy/p2.txt
+
+http://hack-hack.chat.ru/proxy/p3.txt
+
+http://hack-hack.chat.ru/proxy/p4.txt
+
+https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt
+
+https://proxyscrape.com/free-proxy-list
+
+https://spys.one/en/
+
+'''
+
+total = 0
+
+headers = {
+
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
+
+}
+
+if S =='2':
+
+  os.system('clear')
+
+import webbrowser
+
+url = "https://www.instagram.com/hxn_here/"
+
+webbrowser.open(url)
+
+print("""
+
+                _    ___   ___   _
+
+               | |  | \ \ / / \ | |
+
+               | |__| |\ V /|  \| |
+
+               |  __  | > < | . ` |
+
+               | |  | |/ . \| |\  |
+
+               |_|  |_/_/ \_\_| \_|
+
+  _ __  _ __ _____  ___   _
+
+ | '_ \| '__/ _ \ \/ / | | |
+
+ | |_) | | | (_) >  <| |_| |
+
+ | .__/|_|  \___/_/\ _\\__, |
+
+ | |                   __/ |
+
+ |_|                  |___/
+
+              
+
+              Daddy HEXiN is here @hxn_here
+
+               Thx Baby S H , cdiw
+
+""")
+
+with open('proxies.txt', 'w') as proxy_file:
+
+    proxy_file.write('Proxy with file By Daddy HEXiN @hxn_here << insta \n\n')
+
+    proxy_file.close()
+
+for u in urls.splitlines():
+
+    try:
+
+        if u == '':
+
+            pass
+
+        else:
+
+            res = get(str(u),headers=headers)
+
+            proxies = findall(r'\d\d?\d?\.\d\d?\d?\.\d\d?\d?\.\d\d?\d?:\d\d\d?\d?\d?',res.text)
+
+            _len = (len(proxies))
+
+            if int(_len) == 0:
+
+                print(f' Scrapping from: [ {u} ]')
+
+                ip = findall(r'\d\d?\d?\.\d\d?\d?\.\d\d?\d?\.\d\d?\d?', res.text)
+
+                port = findall(r':\d\d\d?\d?\d?', res.text)
+
+                _len2 = len(ip)
+
+                total += _len2
+
+                print(f' New {_len2} proxy scrapped\n')
+
+                for proxy in range(_len2):
+
+                    with open('proxies.txt','a') as proxy_file:
+
+                        try:
+
+                            proxy_file.write(str(ip[proxy]+port[proxy])+'\n')
+
+                        except:
+
+                            pass
+
+            else:
+
+                print(f' Scrapping from: [ {u} ]')
+
+                total += _len
+
+                print(f' New {_len} proxy scrapped\n')
+
+                for _proxy in proxies:
+
+                    with open('proxies.txt', 'a') as proxy_file:
+
+                        proxy_file.write(str(_proxy) + '\n')
+
+    except:
+
+        print(f' Error while scraping [ {u} ]\n')
+
+        pass
+
+input(f' Done Scrapping! total proxies: {total}')
